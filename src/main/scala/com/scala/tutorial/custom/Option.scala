2df97a70f5307c7c243440ece9e378b622480187
jiaming.shang <jiaming.shang@rea-group.com>
2017-08-18 21:45:11 +0800

    Option support filter in for expression

--- src/main/scala/com/scala/tutorial/custom/Option.scala
+++ src/main/scala/com/scala/tutorial/custom/Option.scala
@@ -11,14 +11,37 @@ class OptionOp[A](d:Option[A]){
   def map[B](f:A=>B):Option[B]={
     Option.map(d)(f)
   }
+
+  def flatMap[B](f:A=>Option[B]):Option[B]={
+    Option.flatMap(d)(f)
+  }
+
+  def withFilter(f:A=>Boolean):Option[A]={
+    Option.withFilter(d)(f)
+  }
 }
 
 object Option{
   def map[A,B](o:Option[A])(f:A=>B):Option[B]={
     o match {
       case Some(x) => Some(f(x))
-      case None => None
+      case y => y.asInstanceOf[Option[B]]
     }
   }
+
+  def flatMap[A,B](o:Option[A])(f:A=>Option[B]):Option[B]={
+    o match {
+      case Some(x) => f(x)
+      case y => y.asInstanceOf[Option[B]]
+    }
+  }
+
+  def withFilter[A](o:Option[A])(f:A=>Boolean):Option[A]={
+    o match {
+      case p@Some(x) if f(x) => p
+      case _ => None
+    }
+  }
+
   implicit def toOp[A](o:Option[A]):OptionOp[A] = new OptionOp[A](o)
 }
--- src/test/scala/com/scala/tutorial/custom/OptionSpec.scala
+++ src/test/scala/com/scala/tutorial/custom/OptionSpec.scala
@@ -5,20 +5,60 @@ import org.scalatest._
 /**
   * Created by jiaming.shang on 8/16/17.
   */
-class OptionSpec extends FunSpec with Matchers{
+class OptionSpec extends FunSpec with Matchers {
 
-  describe("Option"){
-    describe("map"){
-      it("should process Some"){
+  describe("Option") {
+    describe("map") {
+      it("should process Some") {
         val some = Some("Test")
-        val result = some.map(x=>x.length)
+        val result = some.map(x => x.length)
         result should be(Some(4))
       }
 
-      it("should ignore None"){
+      it("should ignore None") {
         val none = None
-        val result = none.map((x:String)=>"hello world")
+        val result = none.map((x: String) => "hello world")
         result should be(None)
+      }
+    }
+
+    describe("flatMap") {
+      it("should process Some") {
+        Some("Test").flatMap(x => Some(x.length)) should be(Some(4))
+      }
+
+      it("should ignore None") {
+        None.flatMap((x: String) => Some(x.length)) should be(None)
+      }
+    }
+
+    describe("for") {
+      it("should support for expression") {
+        def getLength(x: String): Option[Int] = Some(x.length)
+        def isOdd(x: Int): Option[Boolean] = Some(x % 2 == 1)
+
+        val odd = for {
+          x <- Some("test")
+          l <- getLength(x)
+          o <- isOdd(l)
+        } yield o
+
+        odd should be(Some(false))
+      }
+
+      it("should support filter") {
+        def getLength(x: String): Option[Int] = Some(x.length)
+        def isOdd(x: Int): Option[Boolean] = Some(x % 2 == 1)
+
+        val test: Option[String] = Some("test")
+
+        val odd = for {
+          x <- test if x.length < 0
+          l <- getLength(x)
+          o <- isOdd(l)
+        } yield o
+
+        odd should be(None)
       }
     }
   }
