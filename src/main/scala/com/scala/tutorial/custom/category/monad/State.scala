038395f77e3bbba7fa25025ca15d8bbd867a5e87
jiaming.shang <jiaming.shang@rea-group.com>
2017-09-16 20:33:29 +0800

    add get,put,modify methods to State.

--- src/main/scala/com/scala/tutorial/custom/category/monad/State.scala
+++ src/main/scala/com/scala/tutorial/custom/category/monad/State.scala
@@ -3,18 +3,30 @@ package com.scala.tutorial.custom.category.monad
 /**
   * Created by jiaming.shang on 9/16/17.
   */
-case class State[S,A](run:S=>(S,A)) {
- def map[B](f:((S,A))=>(S,B)):State[S,B] ={
-   State(s=>f(run(s)))
- }
+case class State[S, A](run: S => (S, A)) {
 
-  def flatMap[B](f:((S,A))=>State[S,B]):State[S,B] ={
-    State(s=>{
+  def apply(s: S): (S, A) = run(s)
+
+  def map[B](f: ((S, A)) => (S, B)): State[S, B] = {
+    State(s => f(run(s)))
+  }
+
+  def flatMap[B](f: ((S, A)) => State[S, B]): State[S, B] = {
+    State(s => {
       val oldState = run(s)
       f(oldState).run(oldState._1)
     })
   }
 }
 
-object State{
+object State {
+  def get[S] = State((s: S) => (s, s))
+
+  def gets[S, A](f: S => A) = State((s: S) => (s, f(s)))
+
+  def put[S](s1: S) = State((s: S) => (s1, Unit))
+
+  def state[S, A](v: A) = State[S, A](s => (s, v))
+
+  def modify[S](f: S => S) = State((s: S) => (f(s), Unit))
 }
--- src/test/scala/com/scala/tutorial/custom/StateSpec.scala
+++ src/test/scala/com/scala/tutorial/custom/StateSpec.scala
@@ -1,5 +1,6 @@
 package com.scala.tutorial.custom
 
+import com.scala.tutorial.custom.category.monad.State
 import org.scalatest.{FunSpec, Matchers}
 
 /**
@@ -16,19 +17,25 @@ class StateSpec extends FunSpec with Matchers{
 
     describe("gets"){
       it("should return the State(s=>(s,f(s)))"){
-        State.gets(x=>x+1)(1) should be((1,2))
+        State.gets[Int,Int](x=>x+1)(1) should be((1,2))
       }
     }
 
     describe("put"){
-      it("should return the State(s=>(f(s),_))"){
-        State.put(x=>x+1)(1) should be((2,_))
+      it("should return the State(s=>(s,_))"){
+        State.put[Int](1)(2) should be((1,Unit))
       }
     }
 
     describe("state"){
       it("should return the State(s=>(_,v))"){
-        State.state(1)(3) should be((_,1))
+        State.state(1)(3) should be((3,1))
+      }
+    }
+
+    describe("modify"){
+      it("should return the State(s=>(f(s),_))"){
+        State.modify[Int](x=>x+1)(1) should be((2,Unit))
       }
     }
   }
