80a1cd2a8f11305e8fd1898ea7774c403ea2b423
jiaming.shang <jiaming.shang@rea-group.com>
2017-09-19 17:19:35 +0800

    support A=>Any interpreter

--- src/main/scala/com/scala/tutorial/custom/category/monad/Free.scala
+++ src/main/scala/com/scala/tutorial/custom/category/monad/Free.scala
@@ -3,31 +3,34 @@ package com.scala.tutorial.custom.category.monad
 /**
   * Created by jiaming.shang on 9/18/17.
   */
-trait Free[S, V] {
-  def flatMap[A](f: V => Free[S, A]): Free[S, A] = {
-    FlatMap(f, this)
+trait Free[S, +V] {
+  def flatMap[A](f: Any => Free[S, A]): Free[S, A] = {
+    FlatMap[S, A](f, this)
   }
 
-  def map[A](f: V => A): Free[S, A] = {
-    Map(f, this)
+  def map[A](f: Any => A): Free[S, A] = {
+    Map[S, A](f, this)
   }
 
-  def run(f: S => V): V = {
+  def run(f: S => Any): Any = {
     this match {
       case Pure(v) => v
-      case Map(mapf, s) => mapf(s.run(f))
-      case FlatMap(flatMapf, s) => flatMapf(s).run(f)
+      case Map(f1, s) => f1(s.run(f))
+      case FlatMap(f2, s) => f2(s.run(f)).run(f)
+      case Mock(s) => f(s)
     }
   }
 }
 
-case class FlatMap[S, V, A](f: V => Free[S, A], s: Free[S, V]) extends Free[S, A]
+case class FlatMap[S, A](f: Any => Free[S, A], s: Free[S, Any]) extends Free[S, A]
 
-case class Map[S, V, A](f: V => A, s: Free[S, V]) extends Free[S, A]
+case class Map[S, A](f: Any => A, s: Free[S, Any]) extends Free[S, A]
 
 case class Pure[S, V](v: V) extends Free[S, V]
+case class Mock[S,V](s:S) extends Free[S,V]
 
 
 object Free {
   def pure[S, V](x: V): Free[S, V] = Pure[S, V](x)
+  def lift[S,V](s:S):Free[S,V]= Mock[S,V](s)
 }
--- src/test/scala/com/scala/tutorial/custom/FreeSpec.scala
+++ src/test/scala/com/scala/tutorial/custom/FreeSpec.scala
@@ -11,20 +11,24 @@ class FreeSpec extends FunSpec with Matchers {
   describe("Free"){
     describe("run"){
       it("should return the correct result"){
-         val f1:Int=>Free[String,Int] = Free[String,Int](s"1${_}")
-         val f2:Int=>Free[String,Int] = Free[String,Int](s"2${_}")
+         val f1:Any=>Free[String,Int] = (x:Any) => {
+           println(x)
+           Free.lift[String,Int](s"1${x}")
+         }
+         val f2:Any=>Free[String,Int] = (x:Any) => {
+           println(x)
+           Free.lift[String,Int](s"2${x}")
+         }
 
-         val interpreter: String=>Int = _.toInt
+         val interpreter: String=>Any = _.toInt
 
          val expressions=for{
-           x1<-Free("1")
+           x1<-Free.lift[String,Int]("1")
            x2<-f1(x1)
            x3<-f2(x2)
          } yield x3
 
          expressions.run(interpreter) should be(211)
-
-
       }
     }
   }
