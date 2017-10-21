227fbaa719b1601b27bf214a7b9a77256fc946c5
jiaming.shang <jiaming.shang@rea-group.com>
2017-10-21 11:06:14 +0800

    refine the Free3 monad

--- src/main/scala/com/scala/tutorial/custom/category/monad/Free3.scala
+++ src/main/scala/com/scala/tutorial/custom/category/monad/Free3.scala
@@ -7,26 +7,12 @@ import com.scala.tutorial.custom.category.monad.Free3.{Id, ~>}
   * Created by jiaming.shang on 10/17/17.
   */
 sealed trait Free3[F[_], A] {
-  def map[B](f: A => B): Free3[F, B] = {
-    this match {
-      case Free3.Pure(v) => Free3.Pure(f(v))
-      case Free3.Impure(v,f1) => Free3.Impure(v,f1.andThen(_.map(f))).asInstanceOf[Free3[F,B]]
-    }
-  }
+  def map[B](f: A => B): Free3[F, B]
 
-  def flatMap[B](f: A => Free3[F, B]): Free3[F, B] = {
-    this match {
-      case Free3.Pure(v) => f(v)
-      case Free3.Impure(v,f1) =>  Free3.Impure(v,f1.andThen(_.flatMap(f))).asInstanceOf[Free3[F,B]]
-    }
-  }
+  def flatMap[B](f: A => Free3[F, B]): Free3[F, B]
 
-  def run(f: F ~> Id): Id[A] = {
-    this match {
-      case Free3.Pure(v) => v
-      case Free3.Impure(v,f1) => f1(f(v)).run(f)
-    }
-  }
+  def run(f: F ~> Id): Id[A]
+
 }
 
 object Free3 {
@@ -35,11 +21,23 @@ object Free3 {
     def apply[A](v: F[A]): G[A]
   }
 
-  type Id[+A] = A
+  type Id[A] = A
 
-  final case class Pure[F[_], A](v: A) extends Free3[F, A]
+  final case class Pure[F[_], A](v: A) extends Free3[F, A] {
+    override def map[B](f: (A) => B): Free3[F, B] = Pure(f(v))
 
-  final case class Impure[F[_], A, B](v: F[A], f: A => Free3[F, B]) extends Free3[F, B]
+    override def flatMap[B](f: (A) => Free3[F, B]): Free3[F, B] = f(v)
+
+    override def run(f: F ~> Id): Id[A] = v
+  }
+
+  final case class Impure[F[_], A, B](v: F[A], f: A => Free3[F, B]) extends Free3[F, B] {
+    override def map[C](f1: (B) => C): Free3[F, C] = Impure[F, A, C](v, (x: A) => f(x).map(f1))
+
+    override def flatMap[C](f1: (B) => Free3[F, C]): Free3[F, C] = Impure[F, A, C](v, (x: A) => f(x).flatMap(f1))
+
+    override def run(f1: F ~> Id): Id[B] = f(f1(v)).run(f1)
+  }
 
   def pure[F[_], A](a: A): Free3[F, A] = Pure(a)
 
