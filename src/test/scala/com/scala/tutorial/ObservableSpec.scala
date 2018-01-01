c1a52fa3bf4984482b666259434919909f9af12f
jiaming.shang <jiaming.shang@rea-group.com>
2018-01-01 17:40:05 +0800

    format code

--- src/test/scala/com/scala/tutorial/ObservableSpec.scala
+++ src/test/scala/com/scala/tutorial/ObservableSpec.scala
@@ -3,19 +3,16 @@ package com.scala.tutorial
 import cats.Eval
 import cats.data.{Reader, Writer}
 import monix.eval.Task
-import org.atnos.eff.{Eff, Fx}
-
 import scala.concurrent.Await
 import org.atnos.eff.syntax.all._
 import org.atnos.eff.all._
+import org.atnos.eff._
 import org.atnos.eff.syntax.addon.monix.task._
 import org.scalatest.{FunSpec, Matchers}
 import monix.execution.Scheduler.Implicits.global
 import monix.reactive._
-import concurrent.duration._
-import cats.data._
-import cats.implicits._
-import cats.syntax.all._
+import scala.concurrent.duration._
+
 /**
   * Created by jiaming.shang on 11/2/17.
   */
@@ -24,21 +21,21 @@ class ObservalbleSpec extends FunSpec with Matchers {
   describe("Observable") {
     it("should run with fix interval") {
 
-      type Stack = Fx.fx5[Either[Throwable, ?], Writer[String,?], Reader[String, ?], Eval,Task]
+      type Stack = Fx.fx5[Either[Throwable, ?], Writer[String, ?], Reader[String, ?], Eval, Task]
 
-      val program:Eff[Stack,Int] = for{
-        _ <- tell[Stack,String](s"Begin program ${System.currentTimeMillis()}")
-        str <- ask[Stack,String]
-        _ <- tell[Stack,String](s"Get config ${System.currentTimeMillis()}")
-        int <- catchNonFatalThrowable[Stack,Int](str.toInt)
-        _ <- tell[Stack,String](s"Done ${System.currentTimeMillis()}")
+      val program: Eff[Stack, Int] = for {
+        _ <- tell[Stack, String](s"Begin program ${System.currentTimeMillis()}")
+        str <- ask[Stack, String]
+        _ <- tell[Stack, String](s"Get config ${System.currentTimeMillis()}")
+        int <- catchNonFatalThrowable[Stack, Int](str.toInt)
+        _ <- tell[Stack, String](s"Done ${System.currentTimeMillis()}")
       } yield int
 
 
-      val task:Task[Either[Throwable,Int]] = program
+      val task: Task[Either[Throwable, Int]] = program
         .runEither
         //        .runWriterUnsafe[String](println(_))
-        .runWriterEval((x:String)=>Eval.later(println(x)))
+        .runWriterEval((x: String) => Eval.later(println(x)))
         .runEval
         .runReader("11")
         .runAsync
