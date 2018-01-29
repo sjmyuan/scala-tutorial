792df20183871037c8459a539ee2c4f3db7c9523
jiaming.shang <jiaming.shang@rea-group.com>
2018-01-29 19:24:09 +0800

    add requirement 3

--- src/test/scala/com/scala/tutorial/discuss/EnumerationAndCaseClassSpec.scala
+++ src/test/scala/com/scala/tutorial/discuss/EnumerationAndCaseClassSpec.scala
@@ -130,6 +130,56 @@ class EnumerationAndCaseClassSpec extends FunSpec with Matchers {
         process(result) should be("Apple is not a animal")
       }
     }
+
+    describe("Requirement3--add meta data") {
+      sealed abstract class Animal(val legNum:Int)
+      final case object Cat extends Animal(4)
+      final case class NotAnimal(value: String) extends Animal(-1)
+      object Animal {
+        def apply(value: String): Animal = value match {
+          case "Cat" => Cat
+          case _ => NotAnimal(value)
+        }
+      }
+
+      def process(v: Either[io.circe.Error, Animal]): String = v match {
+        case Right(Cat) => s"This is cat with ${Cat.legNum} legs"
+        case Right(NotAnimal(v)) => s"${v} is not a animal"
+        case Left(_) => "Error"
+      }
+
+      it("should return This is cat when the type is Cat") {
+        val json =
+          """
+            |{
+            | "type":"Cat"
+            |}
+          """.stripMargin
+
+        val result = for {
+          root <- parse(json)
+          animal <- root.hcursor.get[String]("type")
+        } yield Animal(animal)
+
+        process(result) should be("This is cat with 4 legs")
+      }
+
+      it("should return Apple is not a animal when the type is Apple") {
+        val json =
+          """
+            |{
+            | "type":"Apple"
+            |}
+          """.stripMargin
+
+        val result = for {
+          root <- parse(json)
+          animal <- root.hcursor.get[String]("type")
+        } yield Animal(animal)
+
+        process(result) should be("Apple is not a animal")
+      }
+    }
   }
 
   describe("Enumeration") {
