c78ad801445ab4a931b04c612b2bc54cfe6f7711
jiaming.shang <jiaming.shang@rea-group.com>
2018-01-29 19:16:43 +0800

    add requirement 2

--- src/test/scala/com/scala/tutorial/discuss/EnumerationAndCaseClassSpec.scala
+++ src/test/scala/com/scala/tutorial/discuss/EnumerationAndCaseClassSpec.scala
@@ -55,6 +55,81 @@ class EnumerationAndCaseClassSpec extends FunSpec with Matchers {
         process(result) should be("Apple is not a animal")
       }
     }
+
+    describe("Requirement2--add dog") {
+      sealed trait Animal
+      final case object Cat extends Animal
+      final case object Dog extends Animal
+      final case class NotAnimal(value: String) extends Animal
+      object Animal {
+        def apply(value: String): Animal = value match {
+          case "Cat" => Cat
+          case "Dog" => Dog
+          case _ => NotAnimal(value)
+        }
+      }
+
+      def process(v: Either[io.circe.Error, Animal]): String = v match {
+        case Right(Cat) => "This is cat"
+        case Right(Dog) => "This is dog"
+        case Right(NotAnimal(v)) => s"${v} is not a animal"
+        case Left(_) => "Error"
+      }
+
+      //      def process(v: Either[io.circe.Error, Animal]): String = v match {
+      //        case Right(Cat) => "This is cat"
+      //        case Right(NotAnimal(v)) => s"${v} is not a animal"
+      //        case Left(_) => "Error"
+      //      }
+
+      it("should return This is cat when the type is Cat") {
+        val json =
+          """
+            |{
+            | "type":"Cat"
+            |}
+          """.stripMargin
+
+        val result = for {
+          root <- parse(json)
+          animal <- root.hcursor.get[String]("type")
+        } yield Animal(animal)
+
+        process(result) should be("This is cat")
+      }
+
+      it("should return This is dog when the type is Dog") {
+        val json =
+          """
+            |{
+            | "type":"Dog"
+            |}
+          """.stripMargin
+
+        val result = for {
+          root <- parse(json)
+          animal <- root.hcursor.get[String]("type")
+        } yield Animal(animal)
+
+        process(result) should be("This is dog")
+      }
+
+      it("should return Apple is not a animal when the type is Apple") {
+        val json =
+          """
+            |{
+            | "type":"Apple"
+            |}
+          """.stripMargin
+
+        val result = for {
+          root <- parse(json)
+          animal <- root.hcursor.get[String]("type")
+        } yield Animal(animal)
+
+        process(result) should be("Apple is not a animal")
+      }
+    }
   }
 
   describe("Enumeration") {
@@ -107,5 +182,78 @@ class EnumerationAndCaseClassSpec extends FunSpec with Matchers {
       }
     }
 
+    describe("Requirement2--add dog") {
+      object Animal extends Enumeration {
+        val Cat = Value("Cat")
+        val Dog = Value("Dog")
+      }
+
+//      def process(v: Either[io.circe.Error, Animal.Value]): String = v match {
+//        case Right(Animal.Cat) => "This is cat"
+//        case Right(v@_) => s"${v} is not a animal"
+//        case Left(v@_) => s"Error:${v}"
+//      }
+
+      def process(v: Either[io.circe.Error, Animal.Value]): String = v match {
+        case Right(Animal.Cat) => "This is cat"
+        case Right(Animal.Dog) => "This is dog"
+        case Right(v@_) => s"${v} is not a animal"
+        case Left(v@_) => s"Error:${v}"
+      }
+
+      it("should return This is cat when the type is Cat") {
+        val json =
+          """
+            |{
+            | "type":"Cat"
+            |}
+          """.stripMargin
+
+        implicit val decoder: Decoder[Animal.Value] = Decoder.enumDecoder(Animal)
+
+        val result = for {
+          root <- parse(json)
+          animal <- root.hcursor.get[Animal.Value]("type")
+        } yield animal
+
+        process(result) should be("This is cat")
+      }
+
+      it("should return This is dog when the type is Dog") {
+        val json =
+          """
+            |{
+            | "type":"Dog"
+            |}
+          """.stripMargin
+
+        implicit val decoder: Decoder[Animal.Value] = Decoder.enumDecoder(Animal)
+
+        val result = for {
+          root <- parse(json)
+          animal <- root.hcursor.get[Animal.Value]("type")
+        } yield animal
+
+        process(result) should be("This is dog")
+      }
+
+      ignore("should return Apple is not a animal when the type is Apple") {
+        val json =
+          """
+            |{
+            | "type":"Apple"
+            |}
+          """.stripMargin
+
+        implicit val decoder: Decoder[Animal.Value] = Decoder.enumDecoder(Animal)
+
+        val result = for {
+          root <- parse(json)
+          animal <- root.hcursor.get[Animal.Value]("type")
+        } yield animal
+
+        process(result) should be("This is not a animal")
+      }
+    }
   }
 }
