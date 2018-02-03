8ead1d5be5e5e68bc98d1554f28a18a59631939e
jiaming.shang <jiaming.shang@rea-group.com>
2018-02-03 11:24:38 +0800

    add join and task traverse test

--- src/test/scala/com/scala/tutorial/discuss/ParallelSpec.scala
+++ src/test/scala/com/scala/tutorial/discuss/ParallelSpec.scala
@@ -10,15 +10,30 @@ import scala.util.Random
 class ParallelSpec extends FunSpec with Matchers {
   describe("fs2") {
     describe("0.9.7") {
-      it("should run in parallel") {
-        implicit val strategy = Strategy.fromFixedDaemonPool(10,"test-pool")
-        def Job(): Stream[Task,Int] = Stream.eval(Task[Int] {
-          println(s"${new Date}: ${Thread.currentThread.getName}")
-          Thread.sleep(10)
-          Random.nextInt()
-        })
+      implicit val strategy = Strategy.fromFixedDaemonPool(10, "test-pool")
 
-        Job().merge(Job()).run.unsafeRun()
+      def Job(): Stream[Task, Int] = Stream.eval(Task[Int] {
+        println(s"${System.currentTimeMillis()}: ${Thread.currentThread.getName}")
+        Thread.sleep(1000)
+        Random.nextInt()
+      })
+
+      it("merge should run in parallel") {
+        val result = Job().merge(Job()).runLog.unsafeRun()
+        println(result)
+      }
+
+      it("join should run in parallel") {
+        val stream: Stream[Task, Stream[Task, Int]] = Stream.emits[Task, Int](Range(1, 10)).map(x => Job())
+        concurrent.join(3)(stream).runLog.unsafeRun()
+      }
+
+      it("task should can be run in parallel"){
+        Task.parallelTraverse(Range(1,100))(x=> Task {
+          println(s"${System.currentTimeMillis()}: ${Thread.currentThread.getName}")
+          Thread.sleep(1000)
+          Random.nextInt()
+        }).unsafeRun()
       }
     }
   }
