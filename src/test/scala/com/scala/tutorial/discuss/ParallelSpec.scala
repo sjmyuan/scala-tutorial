8da2cd2463a1743fd48108bb26d1f86517c5dae4
jiaming.shang <jiaming.shang@rea-group.com>
2018-02-04 10:34:28 +0800

    refactor ParallelSpec

--- src/test/scala/com/scala/tutorial/discuss/ParallelSpec.scala
+++ src/test/scala/com/scala/tutorial/discuss/ParallelSpec.scala
@@ -12,51 +12,49 @@ class ParallelSpec extends FunSpec with Matchers {
     describe("0.9.7") {
       implicit val strategy = Strategy.fromFixedDaemonPool(10, "test-pool")
 
-      def Job(): Stream[Task, Int] = Stream.eval(Task[Int] {
+      def Job(): Int = {
         println(s"${System.currentTimeMillis()}: ${Thread.currentThread.getName}")
         Thread.sleep(1000)
         Random.nextInt()
-      })
+      }
 
       it("merge should run in parallel") {
-        val result = Job().merge(Job()).runLog.unsafeRun()
+        val result = Stream.eval(Task {
+          Job()
+        }).merge(Stream.eval(Task {
+          Job()
+        })).runLog.unsafeRun()
         println(result)
       }
 
       it("join should run in parallel") {
-        val stream: Stream[Task, Stream[Task, Int]] = Stream.emits[Task, Int](Range(1, 10)).map(x => Job())
+        val stream: Stream[Task, Stream[Task, Int]] = Stream.emits[Task, Int](Range(1, 10)).map(x => Stream.eval(Task {
+          Job()
+        }))
         concurrent.join(3)(stream).runLog.unsafeRun()
       }
 
-      it("task.parallelTraverse should can be run in parallel"){
-        Task.parallelTraverse(Range(1,100))(x=> Task {
-          println(s"${System.currentTimeMillis()}: ${Thread.currentThread.getName}")
-          Thread.sleep(1000)
-          Random.nextInt()
+      it("task.parallelTraverse should can be run in parallel") {
+        Task.parallelTraverse(Range(1, 100))(x => Task {
+          Job()
         }).unsafeRun()
       }
 
-      it("task.traverse should can be run in seralization"){
-        Task.traverse(Range(1,10))(x=> Task {
-          println(s"${System.currentTimeMillis()}: ${Thread.currentThread.getName}")
-          Thread.sleep(1000)
-          Random.nextInt()
+      it("task.traverse should can be run in seralization") {
+        Task.traverse(Range(1, 10))(x => Task {
+          Job()
         }).unsafeRun()
       }
 
-      it("Task() should use the thread pool"){
+      it("Task() should use the thread pool") {
         Task {
-          println(s"${System.currentTimeMillis()}: ${Thread.currentThread.getName}")
-          Thread.sleep(1000)
-          Random.nextInt()
+          Job()
         }.unsafeRun()
       }
 
-      it("Task.delay should use the main thread"){
+      it("Task.delay should use the main thread") {
         Task.delay {
-          println(s"${System.currentTimeMillis()}: ${Thread.currentThread.getName}")
-          Thread.sleep(1000)
-          Random.nextInt()
+          Job()
         }.unsafeRun()
       }
     }
