131c10eccf18bdcbc185ef20e970b1514c2b5bb0
jiaming.shang <jiaming.shang@rea-group.com>
2018-03-19 23:05:59 +1100

    add Either Effect test

--- src/test/scala/com/scala/tutorial/eff/EitherEffectSpec.scala
+++ src/test/scala/com/scala/tutorial/eff/EitherEffectSpec.scala
@@ -1,26 +1,74 @@
 package com.scala.tutorial.eff
 
 import cats.data.Reader
-import org.atnos.eff.{Eff, Fx}
+import org.atnos.eff._
 import org.scalatest.{FunSpec, Matchers}
 import org.atnos.eff.ReaderEffect._
 import org.atnos.eff.EitherEffect._
 import org.atnos.eff.syntax.all._
 
-class EitherEffectSpec extends FunSpec with Matchers{
-   describe("EitherEffect") {
-     describe("catchLeft") {
-       it("should catch the Left data") {
-         type Stack = Fx.fx2[Reader[Int,?],Either[String,?]]
-         val program:Eff[Stack,Int] = for {
-           number <- ask[Stack,Int]
-           result <- fromEither[Stack,String,Int](Either.cond((number === 0),number,"less than zero"))
-         } yield result
+class EitherEffectSpec extends FunSpec with Matchers {
+  describe("EitherEffect") {
+    describe("catchLeft") {
+      it("should catch the Left data when there is a either effect") {
+        type Stack = Fx.fx2[Reader[Int, ?], Either[String, ?]]
+        val program: Eff[Stack, Int] = for {
+          number <- ask[Stack, Int]
+          result <- fromEither[Stack, String, Int](Either.cond((number === 0), number, "less than zero"))
+        } yield result
 
-         val programAfterTrans = program.catchLeft[String](e=>Eff.pure[Stack,Int](1))
+        val programAfterTrans = program.catchLeft[String](e => Eff.pure[Stack, Int](1))
 
-         programAfterTrans.runReader(2).runEither.run should be(Right(1))
-       }
-     }
-   }
+        programAfterTrans.runReader(2).runEither.run should be(Right(1))
+      }
+
+      it("should not catch the Left data when run catchLeft before translate effect") {
+        sealed trait MyEffect[A]
+        case class transToEither(v: Int) extends MyEffect[Int]
+        type HasMyEffect[R] = MemberIn[MyEffect, R]
+
+        def runMyEffect[R, A, U: Member.Aux[MyEffect, R, ?] : MemberIn[Either[String, ?], ?]](eff: Eff[R, A]): Eff[U, A] = Interpret.translate[R, U, MyEffect, A](eff)(
+          new Translate[MyEffect, U] {
+            override def apply[X](kv: MyEffect[X]): Eff[U, X] = {
+              kv match {
+                case transToEither(v) => fromEither(Either.cond(v === 0, v, "error"))
+              }
+            }
+          })
+
+        type Stack = Fx.fx3[Reader[Int, ?], MyEffect, Either[String, ?]]
+        val program: Eff[Stack, Int] = for {
+          number <- ask[Stack, Int]
+          result <- Eff.send[MyEffect, Stack, Int](transToEither(number))
+        } yield result
+
+        val programAfterTrans = program.catchLeft[String](e => Eff.pure[Stack, Int](1))
+
+        runMyEffect(programAfterTrans.runReader(2)).runEither.run should be(Left("error"))
+      }
+
+      it("should catch the Left data when run catchLeft after translate effect") {
+        sealed trait MyEffect[A]
+        case class transToEither(v: Int) extends MyEffect[Int]
+        type HasMyEffect[R] = MemberIn[MyEffect, R]
+
+        def runMyEffect[R, A, U: Member.Aux[MyEffect, R, ?] : MemberIn[Either[String, ?], ?]](eff: Eff[R, A]): Eff[U, A] = Interpret.translate[R, U, MyEffect, A](eff)(
+          new Translate[MyEffect, U] {
+            override def apply[X](kv: MyEffect[X]): Eff[U, X] = {
+              kv match {
+                case transToEither(v) => fromEither(Either.cond(v === 0, v, "error"))
+              }
+            }
+          })
+
+        type Stack = Fx.fx3[Reader[Int, ?], MyEffect, Either[String, ?]]
+        val program: Eff[Stack, Int] = for {
+          number <- ask[Stack, Int]
+          result <- Eff.send[MyEffect, Stack, Int](transToEither(number))
+        } yield result
+
+        runMyEffect(program.runReader(2)).catchLeft[String](e => Eff.pure[Fx.fx1[Either[String, ?]], Int](1)).runEither.run should be(Right(1))
+      }
+    }
+  }
 }
