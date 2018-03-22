18d78b26f215e7035cb296ccd03378c30ac81bce
jiaming.shang <jiaming.shang@rea-group.com>
2018-03-22 22:10:11 +1100

    add IO test

---
+++ src/test/scala/com/scala/tutorial/cats/effect/IOSpec.scala
@@ -0,0 +1,21 @@
+package com.scala.tutorial.cats.effect
+
+import cats.effect.IO
+import org.scalatest.{FunSpec, Matchers}
+
+class IOSpec extends FunSpec with Matchers {
+  describe("IO") {
+    it("should return Either when there is an exception") {
+      IO(1 / 0).unsafeRunAsync(cb => {
+        cb should matchPattern { case Left(_) => }
+      })
+
+      IO(1 / 0).runAsync(cb => {
+        cb should matchPattern { case Left(_) => }
+        IO.pure(())
+      }).unsafeRunSync()
+
+      IO(1 / 0).attempt.unsafeRunSync() should matchPattern { case Left(_) => }
+    }
+  }
+}
