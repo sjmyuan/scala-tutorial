3c50ca5eb7a6d83902ab455840283a541f36d9fa
jiaming.shang <jiaming.shang@rea-group.com>
2018-08-06 18:52:32 +0800

    add eff last spec

--- src/test/scala/com/scala/tutorial/DoobieSpec.scala
+++ src/test/scala/com/scala/tutorial/DoobieSpec.scala
@@ -12,14 +12,14 @@ class DoobieSpec extends FunSpec with Matchers {
 
   describe("doobie") {
     ignore("can connect to postgresql") {
-      val xa = DriverManagerTransactor[IOLite](
-        "org.postgresql.Driver", "jdbc:postgresql://localhost/postgres", "postgres", "test"
-      )
-
-      val program:ConnectionIO[Int] = sql"select 10".query[Int].unique
-      val result = program.transact(xa).unsafePerformIO
-
-      println(result)
+//      val xa = DriverManagerTransactor[IOLite](
+//        "org.postgresql.Driver", "jdbc:postgresql://localhost/postgres", "postgres", "test"
+//      )
+//
+//      val program:ConnectionIO[Int] = sql"select 10".query[Int].unique
+//      val result = program.transact(xa).unsafePerformIO
+//
+//      println(result)
 
     }
   }
---
+++ src/test/scala/com/scala/tutorial/eff/EffLastSpec.scala
@@ -0,0 +1,27 @@
+package com.scala.tutorial.eff
+
+import cats.data.Reader
+import cats.effect.IO
+import org.atnos.eff.{Eff, Fx}
+import org.scalatest.FunSpec
+import org.atnos.eff.syntax.all._
+import org.atnos.eff.ReaderEffect._
+import org.atnos.eff.addon.cats.effect.IOEffect
+import org.atnos.eff.syntax.addon.cats.effect._
+
+class EffLastSpec extends FunSpec {
+  describe("Eff Last") {
+    it("should run at the last of eff") {
+      type Stack = Fx.fx2[Reader[Int, ?], IO]
+      val program: Eff[Stack, Unit] = for {
+        _ <- IOEffect.ioDelay[Stack,Unit](println("start............"))
+        number <- ask[Stack, Int].addLast(IOEffect.fromIO(IO(println("begin get number"))))
+        result <- IOEffect.ioDelay[Stack, Unit](println(s"get number ${number}"))
+          .addLast(IOEffect.ioDelay[Stack,Unit](println("end get number")))
+        _ <- ask[Stack, Int]
+      } yield result
+
+      program.runReader(10).unsafeRunSync
+    }
+  }
+}
